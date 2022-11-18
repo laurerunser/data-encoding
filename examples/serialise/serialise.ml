3277050958a0c24559b6c5890fbf765ec51350fb
Raphaël Proust <code@bnwr.net>
2022-11-18 11:52:47 +0100

    Simple example

---
+++ examples/serialise/serialise.ml
@@ -0,0 +1,31 @@
+let data =
+  let open Data_encoding.Encoding.Hlist in
+  [ 0L; 4L; 16L; 0xff_ff_ff_ff_ffL ]
+;;
+
+let encoding =
+  let open Data_encoding.Encoding in
+  [ int64; int64; int64; int64 ]
+;;
+
+let buffer = Bytes.create 1024
+
+let to_string e v =
+  match
+    Binary_data_encoding.Backend.write
+      ~dst:buffer
+      ~offset:0
+      ~maximum_length:1024
+      (Data_encoding.Encoding.to_binary e)
+      v
+  with
+  | Error _ -> exit 1
+  | Ok l -> Bytes.sub_string buffer 0 l
+;;
+
+let () =
+  let s = to_string encoding data in
+  let (`Hex s) = Hex.of_string s in
+  print_endline s;
+  exit 0
+;;
