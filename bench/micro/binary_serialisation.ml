3d2d4018398b4655fced4490e2ca4d46287c3c4b
Raphaël Proust <code@bnwr.net>
2022-11-18 10:26:09 +0100

    Add some basic benchmarks

---
+++ bench/micro/binary_serialisation.ml
@@ -0,0 +1,55 @@
+let int64_twople =
+  let open Data_encoding.Encoding in
+  to_binary [ int64; int64 ]
+;;
+
+let int64_triple =
+  let open Data_encoding.Encoding in
+  to_binary [ int64; int64; int64 ]
+;;
+
+let int64_quadle =
+  let open Data_encoding.Encoding in
+  to_binary [ int64; int64; int64; int64 ]
+;;
+
+let offset = 0
+let maximum_length = 8 * 4
+let dst = Bytes.make maximum_length 'x'
+
+let twople ij =
+  Core_bench.Bench.Test.create ~name:"twople" (fun () ->
+      let _ =
+        Binary_data_encoding.Backend.write ~dst ~offset ~maximum_length int64_twople ij
+      in
+      ())
+;;
+
+let triple ijk =
+  Core_bench.Bench.Test.create ~name:"triple" (fun () ->
+      let _ =
+        Binary_data_encoding.Backend.write ~dst ~offset ~maximum_length int64_triple ijk
+      in
+      ())
+;;
+
+let quadle ijkl =
+  Core_bench.Bench.Test.create ~name:"quadle" (fun () ->
+      let _ =
+        Binary_data_encoding.Backend.write ~dst ~offset ~maximum_length int64_quadle ijkl
+      in
+      ())
+;;
+
+let main () =
+  Random.self_init ();
+  let i64 = Random.int64 1024L in
+  Command_unix.run
+    (Core_bench.Bench.make_command
+       [ twople Commons.Hlist.[ i64; i64 ]
+       ; triple Commons.Hlist.[ i64; i64; i64 ]
+       ; quadle Commons.Hlist.[ i64; i64; i64; i64 ]
+       ])
+;;
+
+let () = main ()
---
+++ bench/micro/encoding_translation.ml
@@ -0,0 +1,28 @@
+type any_e = Any : _ Data_encoding.Encoding.t -> any_e
+type any_b = Anyb : _ Binary_data_encoding.Encoding.t -> any_b
+type any_j = Anyj : _ Json_data_encoding.Encoding.t -> any_j
+
+let inputs : (string * any_e) list =
+  let open Data_encoding.Encoding in
+  [ "unit", Any unit; "int64", Any int64; "int64*int64", Any [ int64; int64 ] ]
+;;
+
+let commands =
+  List.map
+    (fun (name, Any encoding) ->
+      Core_bench.Bench.Test.create ~name:("binary_of__" ^ name) (fun () ->
+          Anyb (Data_encoding.Encoding.to_binary encoding)))
+    inputs
+  @ List.map
+      (fun (name, Any encoding) ->
+        Core_bench.Bench.Test.create ~name:("json_of__" ^ name) (fun () ->
+            Anyj (Data_encoding.Encoding.to_json encoding)))
+      inputs
+;;
+
+let main () =
+  Random.self_init ();
+  Command_unix.run (Core_bench.Bench.make_command commands)
+;;
+
+let () = main ()
