0f403cf9322ce851397a5f405c1596d72d34d35b
Raphaël Proust <code@bnwr.net>
2022-12-08 09:36:07 +0100

    expose low-level interface to binary.backend

--- src/binary-data-encoding/backend.mli
+++ src/binary-data-encoding/backend.mli
@@ -1,3 +1,20 @@
+type destination
+
+val mk_destination : ?maximum_length:int -> bytes -> int -> int -> destination
+
+type written =
+  | Written of { destination : destination }
+  | Failed of
+      { destination : destination
+      ; error : string
+      }
+  | Suspended of
+      { destination : destination
+      ; cont : bytes -> int -> int -> written
+      }
+
+val writek : destination -> 'a Encoding.t -> 'a -> written
+
 val write
   :  dst:bytes
   -> offset:int
@@ -7,4 +24,24 @@ val write
   -> (int, int * string) result
 
 val string_of : ?buffer_size:int -> 'a Encoding.t -> 'a -> (string, string) result
+
+type source
+
+val mk_source : ?maximum_length:int -> string -> int -> int -> source
+
+type 'a readed =
+  | Readed of
+      { source : source
+      ; value : 'a
+      }
+  | Failed of
+      { source : source
+      ; error : string
+      }
+  | Suspended of
+      { source : source
+      ; cont : string -> int -> int -> 'a readed
+      }
+
+val readk : source -> 'a Encoding.t -> 'a readed
 val read : src:string -> offset:int -> length:int -> 'a Encoding.t -> ('a, string) result
