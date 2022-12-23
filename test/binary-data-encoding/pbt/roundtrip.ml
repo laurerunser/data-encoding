36172a633e6fea764e604bddd0d80cd648384d74
Raphaël Proust <code@bnwr.net>
2022-12-23 19:47:56 +0100

    a few more pbt tests

--- test/binary-data-encoding/pbt/roundtrip.ml
+++ test/binary-data-encoding/pbt/roundtrip.ml
@@ -3,6 +3,10 @@ type any_encoding = AnyE : string * _ Binary_data_encoding.Encoding.t -> any_enc
 let testables =
   [ AnyE ("unit", Binary_data_encoding.Encoding.unit)
   ; AnyE ("i64", Binary_data_encoding.Encoding.int64)
+  ; AnyE ("ui64", Binary_data_encoding.Encoding.uint64)
+  ; AnyE ("i32", Binary_data_encoding.Encoding.int32)
+  ; AnyE ("ui32", Binary_data_encoding.Encoding.uint32)
+  ; AnyE ("ui8", Binary_data_encoding.Encoding.uint8)
   ; AnyE ("i64,opt(i32)", Binary_data_encoding.Encoding.[ int64; option int32 ])
   ]
 ;;
