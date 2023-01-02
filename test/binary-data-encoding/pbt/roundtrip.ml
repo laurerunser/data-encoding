848a4f096cfc08cb38467757f313d914ace90f7c
Raphaël Proust <code@bnwr.net>
2023-01-02 15:29:36 +0100

    binary.test.pbt more thourough

--- test/binary-data-encoding/pbt/roundtrip.ml
+++ test/binary-data-encoding/pbt/roundtrip.ml
@@ -1,21 +1,21 @@
-type any_encoding = AnyE : string * _ Binary_data_encoding.Encoding.t -> any_encoding
-
-let testables =
-  [ AnyE ("unit", Binary_data_encoding.Encoding.unit)
-  ; AnyE ("i64", Binary_data_encoding.Encoding.int64)
-  ; AnyE ("ui64", Binary_data_encoding.Encoding.uint64)
-  ; AnyE ("i32", Binary_data_encoding.Encoding.int32)
-  ; AnyE ("ui32", Binary_data_encoding.Encoding.uint32)
-  ; AnyE ("ui8", Binary_data_encoding.Encoding.uint8)
-  ; AnyE ("i64,opt(i32)", Binary_data_encoding.Encoding.[ int64; option int32 ])
-  ; AnyE ("str[ui8]", Binary_data_encoding.Encoding.string `UInt8)
-  ; AnyE
-      ("str[10]", Binary_data_encoding.Encoding.string (`Fixed (Stdint.Uint32.of_int 10)))
-  ]
+let () =
+  Seq.iter
+    (fun (Testable.AnyE (name, t)) -> QCheck2.Test.check_exn (Pbtlib.to_test name t))
+    Testable.basics
 ;;
 
 let () =
-  List.iter
-    (fun (AnyE (name, t)) -> QCheck2.Test.check_exn (Pbtlib.to_test name t))
-    testables
+  (* sampling the long long not-so-basic sequence because it's too long *)
+  let prng = Random.State.make [| 111; 1111; 11111 |] in
+  let rec skips s () =
+    let n = Random.State.int prng 64 in
+    let s = Seq.drop n s in
+    match s () with
+    | Seq.Nil -> Seq.Nil
+    | Seq.Cons (v, s) -> Seq.Cons (v, skips s)
+  in
+  let not_so_basic = skips Testable.not_so_basic in
+  Seq.iter
+    (fun (Testable.AnyE (name, t)) -> QCheck2.Test.check_exn (Pbtlib.to_test name t))
+    not_so_basic
 ;;
---
+++ test/binary-data-encoding/pbt/testable.ml
@@ -0,0 +1,88 @@
+type any_encoding = AnyE : string * _ Binary_data_encoding.Encoding.t -> any_encoding
+
+let all_ground_encodings : any_encoding Seq.t =
+  let open Binary_data_encoding.Encoding in
+  List.to_seq
+    [ AnyE ("unit", unit)
+    ; AnyE ("i64", int64)
+    ; AnyE ("ui64", uint64)
+    ; AnyE ("i32", int32)
+    ; AnyE ("ui32", uint32)
+    ; AnyE ("ui16", uint16)
+    ; AnyE ("ui8", uint8)
+    ; AnyE ("str[ui8]", Binary_data_encoding.Encoding.string `UInt8)
+      (* TODO: support bigger strings *)
+    ]
+;;
+
+let str_encodings : any_encoding Seq.t =
+  Seq.append
+    (Seq.map
+       (fun n ->
+         AnyE
+           ( Format.asprintf "str[%d]" n
+           , Binary_data_encoding.Encoding.string (`Fixed (Stdint.Uint32.of_int n)) ))
+       (List.to_seq [ 0; 1; 7; 129 ]))
+    (Seq.map
+       (fun n ->
+         AnyE
+           ( Format.asprintf "bytes[%d]" n
+           , Binary_data_encoding.Encoding.bytes (`Fixed (Stdint.Uint32.of_int n)) ))
+       (List.to_seq [ 0; 1; 7; 129 ]))
+;;
+
+let simplish_encodings : any_encoding Seq.t -> any_encoding Seq.t =
+ fun s ->
+  Seq.concat_map
+    (fun (AnyE (s, e)) ->
+      List.to_seq
+        [ AnyE ("opt(" ^ s ^ ")", Binary_data_encoding.Encoding.option e)
+        ; AnyE
+            ( "conv[id](" ^ s ^ ")"
+            , Binary_data_encoding.Encoding.conv
+                ~serialisation:Fun.id
+                ~deserialisation:Result.ok
+                e )
+        ; AnyE
+            ( "headered[unit](" ^ s ^ ")"
+            , Binary_data_encoding.Encoding.(
+                with_header unit (fun _ -> Ok ()) (fun () -> Ok e))
+                (Pbtlib.equal_of_encoding e) )
+        ])
+    s
+;;
+
+let lvl0 = Seq.append all_ground_encodings str_encodings
+let lvl1 = simplish_encodings lvl0
+let all_simplish_encodings = Seq.append lvl0 lvl1
+let lvl2 = simplish_encodings lvl1
+let basics = Seq.append all_simplish_encodings lvl2
+
+type any_hlist_encoding =
+  | AnyH :
+      string * _ Binary_data_encoding.Encoding.Hlist.t Binary_data_encoding.Encoding.t
+      -> any_hlist_encoding
+
+let tuplify : any_encoding -> any_hlist_encoding =
+ fun (AnyE (s, e)) -> AnyH ("[" ^ s ^ "]", [ e ])
+;;
+
+let detuplify : any_hlist_encoding -> any_encoding = fun (AnyH (s, e)) -> AnyE (s, e)
+
+let tuple_encodings
+    : any_encoding Seq.t -> any_hlist_encoding Seq.t -> any_hlist_encoding Seq.t
+  =
+ fun es hs ->
+  Seq.concat_map
+    (fun (AnyH (hs, h)) ->
+      Seq.map (fun (AnyE (es, e)) -> AnyH (es ^ ";" ^ hs, [ e; h ])) es)
+    hs
+;;
+
+let not_so_basic : any_encoding Seq.t =
+  let lvl3 =
+    tuple_encodings all_simplish_encodings (Seq.map tuplify all_simplish_encodings)
+  in
+  let lvl4 = tuple_encodings all_simplish_encodings lvl3 in
+  Seq.map detuplify (Seq.append lvl3 lvl4)
+;;
