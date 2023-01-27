0226bbdb3849db3cc52513f18a701697434dd2c6
Raphaël Proust <code@bnwr.net>
2023-01-27 14:44:22 +0100

    Use the QCheck runner for the tests (better reporting)

--- test/binary-data-encoding/pbt/roundtrip.ml
+++ test/binary-data-encoding/pbt/roundtrip.ml
@@ -1,10 +1,4 @@
-let () =
-  Seq.iter
-    (fun (Testable.AnyE (name, t)) -> QCheck2.Test.check_exn (Pbtlib.to_test name t))
-    Testable.basics
-;;
-
-let prng =
+let seed_gen =
   match Sys.getenv_opt "PBT_ROUNDTRIP_SEED" with
   | None ->
     let prng = Random.State.make_self_init () in
@@ -19,17 +13,29 @@ let prng =
     | Some seed -> Random.State.make [| seed |])
 ;;
 
-let () =
+let run tests =
+  let rand = Random.State.make [| Random.State.int seed_gen (1 lsl 29) |] in
+  let exitcode =
+    QCheck_runner.run_tests
+      ~rand
+      (List.of_seq
+         (Seq.map (fun (Testable.AnyE (name, t)) -> Pbtlib.to_test name t) tests))
+  in
+  if exitcode <> 0 then exit exitcode
+;;
+
+let () = run Testable.basics
+
+let not_so_basic =
   (* sampling the long long not-so-basic sequence because it's too long *)
   let rec skips s () =
-    let n = Random.State.int prng 128 in
+    let n = Random.State.int seed_gen 128 in
     let s = Seq.drop (n + 1) s in
     match s () with
     | Seq.Nil -> Seq.Nil
     | Seq.Cons (v, s) -> Seq.Cons (v, skips s)
   in
-  let not_so_basic = skips Testable.not_so_basic in
-  Seq.iter
-    (fun (Testable.AnyE (name, t)) -> QCheck2.Test.check_exn (Pbtlib.to_test name t))
-    not_so_basic
+  skips Testable.not_so_basic
 ;;
+
+let () = run not_so_basic
--- test/json-data-encoding/pbt/roundtrip.ml
+++ test/json-data-encoding/pbt/roundtrip.ml
@@ -1,3 +1,18 @@
+let seed_gen =
+  match Sys.getenv_opt "PBT_ROUNDTRIP_SEED" with
+  | None ->
+    let prng = Random.State.make_self_init () in
+    let seed = Random.State.int prng (1 lsl 29) in
+    Printf.printf "Seed picked randomly: PBT_ROUNDTRIP_SEED=%d\n" seed;
+    Random.State.make [| seed |]
+  | Some s ->
+    (match int_of_string_opt s with
+    | None ->
+      Printf.eprintf "Malformed PBT_ROUNDTRIP_SEED";
+      exit 1
+    | Some seed -> Random.State.make [| seed |])
+;;
+
 type any_encoding = AnyE : _ Json_data_encoding.Encoding.t -> any_encoding
 
 let testables =
@@ -16,4 +31,12 @@ let testables =
   ]
 ;;
 
-let () = List.iter (fun (AnyE t) -> QCheck2.Test.check_exn (Pbtlib.to_test t)) testables
+let run tests =
+  let rand = Random.State.make [| Random.State.int seed_gen (1 lsl 29) |] in
+  let exitcode =
+    QCheck_runner.run_tests ~rand (List.map (fun (AnyE t) -> Pbtlib.to_test t) tests)
+  in
+  if exitcode <> 0 then exit exitcode
+;;
+
+let () = run testables
